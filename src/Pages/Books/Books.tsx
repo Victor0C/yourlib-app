5f29004066eaa0bbe775e3458a183e464993b939
Victor Hugo <hugovco511@gmail.com>
2025-03-22 22:02:38 -0300

    feat: implementar listagem de livros com busca e tratamento de erros

--- src/Pages/Books/Books.tsx
+++ src/Pages/Books/Books.tsx
@@ -1,3 +1,4 @@
+import { useMyToastPromise } from '@/components/MyToasts';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
@@ -10,62 +11,133 @@ import {
 	TableHeader,
 	TableRow,
 } from '@/components/ui/table';
+import { Book, getBooks } from '@/services/Books';
 import { Eye, Pencil, Plus, Search, Trash2 } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 const Books = () => {
+	const [books, setBooks] = useState<Book[]>([]);
+	const [requestBooks] = useState<boolean>(true);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [search, setSearch] = useState<boolean>('');
+	const toastPromise = useMyToastPromise();
+
+	useEffect(() => {
+		setIsLoading(true);
+		toastPromise(
+			getBooks(),
+			(data) => {
+				setBooks(data);
+				setIsLoading(false);
+				return 'Registros coletados';
+			},
+			(error) => {
+				setIsLoading(false);
+				if (error instanceof Error) {
+					return error.message;
+				}
+				return 'Erro desconhecido';
+			}
+		);
+	}, [requestBooks, search]);
+
 	return (
 		<Card className='w-full h-auto card bg-[#1F2328] border-[#BD8D4C] border-2 py-2 px-2 '>
 			<div className='flex w-full items-center space-x-2'>
-				<Input
-					className='text-[#d8d6d2] bg-[#282C34] border-[#BD8D4C]
-												focus:border-[#BD8D4C] focus:ring-[#BD8D4C]'
-					type='text'
-					placeholder='Pesquise um livro'
-				/>
+				<form
+					className='flex w-full items-center'
+					onSubmit={(e) => {
+						e.preventDefault();
+						setSearch(e.currentTarget.search.value);
+					}}>
+					<Input
+						name='search'
+						className='text-[#d8d6d2] bg-[#282C34] border-[#BD8D4C]
+              focus:border-[#BD8D4C] focus:ring-[#BD8D4C]'
+						type='text'
+						placeholder='Pesquise um livro'
+						disabled={isLoading}
+					/>
+					<Button
+						type='submit'
+						className='bg-[#BD8D4C] text-[#1F2328] hover:bg-[#A77B3B] 
+            font-bold transition-colors disabled:opacity-50 ml-2'
+						disabled={isLoading}>
+						<Search />
+					</Button>
+				</form>
 				<Button
-					type='submit'
+					type='button'
 					className='bg-[#BD8D4C] text-[#1F2328] hover:bg-[#A77B3B] 
-                font-bold transition-colors disabled:opacity-50'>
-					<Search />
-				</Button>
-				<Button
-					type='submit'
-					className='bg-[#BD8D4C] text-[#1F2328] hover:bg-[#A77B3B] 
-                font-bold transition-colors disabled:opacity-50'>
+          font-bold transition-colors disabled:opacity-50'
+					disabled={isLoading}>
 					<Plus />
 				</Button>
 			</div>
-			<Table>
-				<TableCaption className='text-[#f1e2e2] text-sm'>
-					Você tem 10 livros cadastrados
-				</TableCaption>
-				<TableHeader>
-					<TableRow className='hover:bg-transparent'>
-						<TableHead className='text-[#f1e2e2] w-9/12'>Título</TableHead>
-						<TableHead className='text-[#f1e2e2] text-center'>Status</TableHead>
-						<TableHead className='text-[#f1e2e2] text-center'>
-							Condição
-						</TableHead>
-						<TableHead className='text-[#f1e2e2] text-center'>Ações</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody className='text-[#f1e2e2]'>
-					<TableRow className='hover:bg-transparent'>
-						<TableCell className='font-medium w-9/12'>
-							Snehor dos Aneis
-						</TableCell>
-						<TableCell className='text-center'>Disponivel</TableCell>
-						<TableCell className='text-center'>Bom</TableCell>
-						<TableCell className='text-center'>
-							<div className='flex justify-center items-center space-x-3'>
-								<Eye size={20} />
-								<Pencil size={17} />
-								<Trash2 size={18} />
-							</div>
-						</TableCell>
-					</TableRow>
-				</TableBody>
-			</Table>
+			{books.length === 0 ? (
+				<p className='text-[#f1e2e2] text-center my-4'>
+					Nenhum livro encontrado
+				</p>
+			) : (
+				<Table>
+					<TableCaption className='text-[#f1e2e2] text-sm'>
+						Você tem 10 livros cadastrados
+					</TableCaption>
+					<TableHeader>
+						<TableRow className='hover:bg-transparent'>
+							<TableHead className='text-[#f1e2e2] w-9/12'>Título</TableHead>
+							<TableHead className='text-[#f1e2e2] text-center'>
+								Status
+							</TableHead>
+							<TableHead className='text-[#f1e2e2] text-center'>
+								Condição
+							</TableHead>
+							<TableHead className='text-[#f1e2e2] text-center'>
+								Ações
+							</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody className='text-[#f1e2e2]'>
+						{books.map((book: Book) => (
+							<TableRow className='hover:bg-transparent'>
+								<TableCell className='font-medium w-9/12'>
+									{book.title}
+								</TableCell>
+								<TableCell className='text-center'>{book.status}</TableCell>
+								<TableCell className='text-center'>{book.condition}</TableCell>
+								<TableCell className='text-center'>
+									<div className='flex justify-center items-center space-x-3'>
+										<Eye
+											size={20}
+											className={`${
+												isLoading
+													? 'opacity-50'
+													: 'cursor-pointer hover:text-[#BD8D4C] transition-colors'
+											}`}
+										/>
+										<Pencil
+											size={17}
+											className={`${
+												isLoading
+													? 'opacity-50'
+													: 'cursor-pointer hover:text-[#BD8D4C] transition-colors'
+											}`}
+										/>
+										<Trash2
+											size={18}
+											className={`${
+												isLoading
+													? 'opacity-50'
+													: 'cursor-pointer hover:text-[#BD8D4C] transition-colors'
+											}`}
+										/>
+									</div>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			)}
 		</Card>
 	);
 };
---
+++ src/services/Books.ts
@@ -0,0 +1,51 @@
+import { AxiosError } from 'axios';
+import api from './axios';
+
+interface Book {
+	title: string;
+	authors: string[];
+	pages: number;
+	genres: string[];
+	status: string;
+	condition: string;
+	description: string;
+}
+
+async function getBooks() {
+	try {
+		const { data } = await api.get<Book[]>('/users/books');
+		return data;
+	} catch (error) {
+		if (
+			error instanceof AxiosError &&
+			error.response &&
+			error.response.status >= 400 &&
+			error.response.status < 500
+		) {
+			throw new Error('Erro ao cadastrar o livro');
+		}
+
+		throw new Error('Erro desconhecido');
+	}
+}
+
+async function createBook(book: Book) {
+	try {
+		const { data } = await api.post<Book>('/users/books', book);
+		return data;
+	} catch (error) {
+		if (
+			error instanceof AxiosError &&
+			error.response &&
+			error.response.status >= 400 &&
+			error.response.status < 500
+		) {
+			throw new Error('Erro ao cadastrar o livro');
+		}
+
+		throw new Error('Erro desconhecido');
+	}
+}
+
+export { createBook, getBooks };
+export type { Book };
